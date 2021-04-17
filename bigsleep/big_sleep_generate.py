08ef878372b5db668666a60187d1bb2f9bc20c48
Sara Sisti <sara.sisti@outlook.it>
2021-04-17 16:00:21 +0200

    first draft big sleep

---
+++ bigsleep/big_sleep_generate.py
@@ -0,0 +1,10 @@
+from big_sleep import Imagine
+
+dream = Imagine(
+    text="psychedelic hallucination of many eyes in a field of colorful flowers",
+    lr=5e-2,
+    save_every=25,
+    save_progress=True
+)
+
+dream()
